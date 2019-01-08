45d16201c01e0dc5bf2a693394e229952d663f98
shannon-nabors <shannon.nabors@gmail.com>
2019-01-08 17:08:56 -0500

    added methods to return films for a character, accounting for a variety of errors

--- bin/run.rb
+++ bin/run.rb
@@ -5,4 +5,7 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
-show_character_movies(character)
+
+while show_character_movies(character).nil?
+  character = get_character_from_user
+end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,15 @@
+require 'pry'
+
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the Star Wars API!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character name:"
+  input = gets.chomp.downcase
+  if input.empty?
+    get_character_from_user
+  else
+    return input
+  end
 end
