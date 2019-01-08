f36268885d6eafc7b0d97b6874fd38389a529755
Ben Yellin <byellindc@gmail.com>
2019-01-08 14:26:56 -0500

    give me that green check!

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,32 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
+def run
+  input = choose_menu
+
+  if input == 'c'
+    puts "\n"
+    character = get_character_from_user
+    show_character_movies(character)
+  elsif input == 'm' || input == 'f'
+    puts "\n"
+    movie = get_movie_from_user
+    show_movie_info(movie)
+  elsif input == 'a'
+  end
+
+  return input
+  
+  # if !['c','m', 'f'].include?(input)
+  #   exit_cli
+  # end
+end
+
 welcome
-character = get_character_from_user
-show_character_movies(character)
+input = nil
+while input != 'e'
+  input = run
+end
+
+# character = get_character_from_user
+# show_character_movies(character)
