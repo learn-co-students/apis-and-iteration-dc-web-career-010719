ca93fa53122f9e55a207518b99635645d28a9ca6
Ross Jones <rossmosis.jones@gmail.com>
2019-01-08 15:36:57 -0500

    welcomes and promts user, then finds and prints movies of character

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars universe.\n\n"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please enter a character name\n\n"
   # use gets to capture the user's input. This method should return that input, downcased.
+  character_name = gets.chomp
 end
