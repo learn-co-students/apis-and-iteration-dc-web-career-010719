65b485221b2de9f320b561bb5db6142e0a34d8bd
github username <metelskyi88@gmail.com>
2019-01-08 15:59:42 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,12 +1,19 @@
 require 'rest-client'
 require 'json'
 require 'pry'
-
-def get_character_movies_from_api(character_name)
-  #make the web request
+def get_character_json
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+end
+def get_character_movies_from_api(character_name)
+response_hash = get_character_json
+   response_hash["results"].each do |key|
+     if key["name"] == character_name
+       return key["films"]
+     else return false
+     end
+   end
+end   
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,18 +23,22 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |film|
+    response_string = RestClient.get(film)
+    response_hash = JSON.parse(response_string)
+    puts response_hash["title"]
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
+if !films
+ return false
+else
   print_movies(films)
+return true
+  end
 end
-
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,15 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Please, enter a valid character name!"
   # use gets to capture the user's input. This method should return that input, downcased.
+      input = gets.chomp
+    while input.empty?
+      puts "Input is not valid. Try again!"
+      input = gets.chomp
+    end
+    return input
 end
